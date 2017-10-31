4717ce8798cce00ac292a68f3336a684e4e468f4
Manjunath R <mramasw2@cisco.com>
2017-10-31 13:11:38 +0530

    commited first1

---
+++ Manjunath_Better/src/test/java/com/wip/util/D.java
@@ -0,0 +1,19 @@
+package com.wip.util;
+
+public class D {
+	
+	D( String name)
+	{
+		System.out.println("My Name is " + name);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		D d= new D("QSI HEALTHCARE");
+		
+
+	}
+
+}
